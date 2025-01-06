b07f93776c61a9683c6dcdef03eb971d2d1bf509
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-01-06 15:47:55 -0300

    feat: add project

---
+++ ApiTopicoAlura/src/main/java/ApiTopicoAlura/ApiTopicoAlura/ApiTopicoAluraApplication.java
@@ -0,0 +1,13 @@
+package ApiTopicoAlura.ApiTopicoAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTopicoAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTopicoAluraApplication.class, args);
+	}
+
+}
---
+++ ApiTopicoAlura/src/test/java/ApiTopicoAlura/ApiTopicoAlura/ApiTopicoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiTopicoAlura.ApiTopicoAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTopicoAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
